8d80e2285f10a2da02ccce340b529f8e7859cbbe
Erick Rivera <yomellamoerick.771@gmail.com>
2023-07-14 19:57:45 -0400

    manual scaffold and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
